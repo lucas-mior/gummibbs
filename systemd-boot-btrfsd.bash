04debe65ddd61e28f57bbb6ac6e75e8589ec8bbd
lucas-mior <lucas.mior@tutamail.com>
2025-09-30 10:05:57 -0300

    wip

--- systemd-boot-btrfsd.bash
+++ systemd-boot-btrfsd.bash
@@ -193,6 +193,9 @@ find /.snapshots/ -mindepth 2 -maxdepth 2 \
         -k "/mnt/$script/vmlinuz-$kernel_type" \
         -g "/mnt/$script/initramfs-$kernel_type.img"
 
+    umount "$snapshot"
+    umount "$snapshot/mnt"
+
     initramfs=$(savefrom "/tmp/$script/initramfs-$kernel_type.img")
     initramfs=$(echo "$initramfs" | sed 's|/boot/||')
     if [ -z "$initramfs" ]; then
