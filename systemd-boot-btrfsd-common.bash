78616f3f05a1d666e4c5edcc0398976416af6bc0
lucas-mior <lucas.mior@tutamail.com>
2025-10-01 11:56:21 -0300

    wip

---
+++ systemd-boot-btrfsd-common.bash
@@ -0,0 +1,36 @@
+# shellcheck disable=SC2034
+
+export LC_ALL=C
+shopt -s nullglob
+
+error () {
+    >&2 printf "$@"
+    return
+}
+
+if [ ! -f /etc/os-release ] || ! grep -q '^ID=arch' /etc/os-release; then
+    error "Not running Arch Linux. Exiting...\n"
+    exit 1
+fi
+
+if ! bootctl status >/dev/null 2>&1; then
+    error "Not using systemd-boot. Exiting...\n"
+    exit 1
+fi
+
+if test -n "$(find /boot/ -maxdepth 1 -iname "*.efi" -print -quit)"; then
+    error "Unified kernel images detected in /boot. Exiting...\n"
+    exit 1
+fi
+
+if command -v dracut >/dev/null 2>&1; then
+    error "Dracut detected. Exiting..."
+    exit 1
+fi
+
+snapshots="/.snapshots/"
+
+if btrfs subvol show / | head -n 1 | grep -Eq -- "$snapshots"; then
+    error "Snapshot mounted. Exiting...\n"
+    exit 1
+fi
